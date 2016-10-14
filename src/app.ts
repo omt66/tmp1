98a6d034642dab38cc1aaadec2f4121322c9abc5
Ogun <omt66@yahoo.com>
2016-10-13 18:52:23 -0700

    misc temp files

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+//our root app component
+import {Component, NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div>
+      <h2>Hello {{name}}</h2>
+      {{tstInfo}}
+    </div>
+  `,
+})
+export class App {
+  name:string;
+  tstInfo = "OT: Hello this is a test :)";
+  
+  constructor() {
+    this.name = 'Angular2'
+  }
+}
+
+@NgModule({
+  imports: [ BrowserModule ],
+  declarations: [ App ],
+  bootstrap: [ App ]
+})
+export class AppModule {}
